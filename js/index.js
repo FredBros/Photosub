65e4e4389ceedd3a0bb230ff69ab99ec59a5e0dd
FredBros <fred.b.devweb@gmail.com>
2021-09-18 21:04:45 +0200

    Articles ok
    Articles ok
    navbar ok
    smooth scroll CSS... à refaire en JS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+//
+// Hamburger menu
+//
 const navIcon = document.querySelector('#nav-icon');
 const navMenu = document.querySelector('.nav-menu');
 
@@ -5,8 +8,39 @@ navIcon.addEventListener('click', () => navMenu.classList.toggle('display-on'));
 
 
 window.addEventListener('click', function(e){
-	// var box = document.getElementById('box1');
 	if (!navMenu.contains(e.target) && (!navIcon.contains(e.target))){
         navMenu.classList.remove('display-on');
     }
 });
+
+
+// 
+// NAVBAR SCROLL
+//
+// const navItems = document.getElementsByClassName('nav-item');
+// console.log (navItems);
+
+
+
+// Array.from(navItems).forEach(function(element, i) {
+//     element.addEventListener('click', (e) => {
+//         const btn = Array.prototype.indexOf.call(navItems,e);
+//         console.log (e);
+        
+//     });
+// });
+
+function scrollFromMenu(e){
+    let scrollY = e * window.innerHeight;
+    window.scrollTo({top: scrollY, behavior: 'smooth'})
+    }
+
+// 
+// 
+// CAROUSEL
+// from Splide
+// 
+
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#image-slider' ).mount();
+} );
