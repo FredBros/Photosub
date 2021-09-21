4050357f55a3d3e780a5fbad94146c804fb9b8a3
FredBros <fred.b.devweb@gmail.com>
2021-09-21 21:51:03 +0200

    Index.html ok
    [FEAT] Responsive : ok
    [FEAT] Animations : ok
    To-do : event page

--- js/index.js
+++ js/index.js
@@ -33,3 +33,67 @@ document.addEventListener( 'DOMContentLoaded', function () {
         height : '100vh',
     } ).mount();
 } );
+// 
+// In & Out Articles & Form effect
+// (index page)
+// 
+const articles = document.querySelectorAll('.index-section article');
+const form = document.querySelector('form');
+const footer = document.querySelector('footer');
+
+// Test resize Trigger
+window.addEventListener('resize', resetArticleTransformForSmartphone);
+function resetArticleTransformForSmartphone(){
+    if (window.innerWidth < 768){
+    // Smartphone mode
+    articles.forEach(e => e.style.transform = 'none')    
+} else {
+    // Large screen Mode
+    articles.forEach(e => checkArticlePosition(e));
+}};
+
+// Test Scoll Trigger
+// Article & Form in out
+window.addEventListener("scroll", () => {
+if (window.matchMedia("(min-width: 768px)").matches) {
+    checkFormPosition();
+    articles.forEach(e => checkArticlePosition(e));
+}
+});
+
+
+function checkArticlePosition(article){
+    let fromTop = article.getBoundingClientRect().top;
+    let hWindow = window.innerHeight;
+    let hPercent = Math.floor((fromTop / hWindow) * 100);
+    if (hPercent < 80 && hPercent > 20) {
+        articleIn(article);
+    } else {
+        articleOut(article);
+    };
+}
+function articleIn(article){
+    article.classList.contains('article-right') ? article.style.transform = 'translate(-100%, -50%)' : article.style.transform = 'translate(100%, -50%)';
+}
+function articleOut(article){
+    article.style.transform = 'translate(0, -50%)';
+}
+
+// 
+// In & Out Form Scrolling effect
+// 
+function checkFormPosition(){
+//     const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+//     if (scrollTop+clientHeight === scrollHeight) {
+//         form.style.transform = 'translate(0, -100%)';
+//     }
+// }
+let fromTop = footer.getBoundingClientRect().top;
+    let hWindow = window.innerHeight;
+    let hPercent = Math.floor((fromTop / hWindow) * 100);
+    if (hPercent < 95) {
+        form.style.transform = 'translate(0, 0)';
+    } else {
+        form.style.transform = 'translate(0, 100%)';
+    }
+};
