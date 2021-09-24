323393576da7999d3a834a93f24eed4677ad3065
FredBros <fred.b.devweb@gmail.com>
2021-09-24 12:57:11 +0200

    Deployment Squash
    Production
    
    Production 2
    
    [FIX] path CSS file
    
    Production 3 :(
    
    [FIX] JS files path
    
    Production
    
    Production 2

--- js/index.js
+++ js/index.js
@@ -1,35 +1,4 @@
 //
-// Hamburger menu
-//
-const navIcon = document.querySelector("#nav-icon");
-const navMenu = document.querySelector(".nav-menu");
-
-navIcon.addEventListener("click", () => navMenu.classList.toggle("display-on"));
-
-window.addEventListener("click", function (e) {
-  if (!navMenu.contains(e.target) && !navIcon.contains(e.target)) {
-    navMenu.classList.remove("display-on");
-  }
-});
-//
-//
-// Cick on Navigation Bar
-//
-const arrayNav = ["home", "team", "work", "news", "gallery", "contact"];
-const navItems = Array.from(document.querySelectorAll(".nav-item"));
-
-navItems.forEach((navItem) =>
-  navItem.addEventListener("click", (e) => {
-    if (window.location.pathname !== "/index.html") {
-      window.location.href =
-        "/index.html#" + arrayNav[navItems.indexOf(e.target)];
-    } else {
-      window.location.href = "#" + arrayNav[navItems.indexOf(e.target)];
-    }
-  })
-);
-
-//
 //
 // CAROUSEL
 // from Splide
