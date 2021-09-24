1f324263af3be81cc932d29656ef05ab26ebcfdb
FredBros <fred.b.devweb@gmail.com>
2021-09-24 09:07:09 +0200

    Navbar ok
    [FIX] navbar links

--- js/index.js
+++ js/index.js
@@ -1,99 +1,105 @@
 //
 // Hamburger menu
 //
-const navIcon = document.querySelector('#nav-icon');
-const navMenu = document.querySelector('.nav-menu');
+const navIcon = document.querySelector("#nav-icon");
+const navMenu = document.querySelector(".nav-menu");
 
-navIcon.addEventListener('click', () => navMenu.classList.toggle('display-on'));
+navIcon.addEventListener("click", () => navMenu.classList.toggle("display-on"));
 
-
-window.addEventListener('click', function(e){
-	if (!navMenu.contains(e.target) && (!navIcon.contains(e.target))){
-        navMenu.classList.remove('display-on');
-    }
+window.addEventListener("click", function (e) {
+  if (!navMenu.contains(e.target) && !navIcon.contains(e.target)) {
+    navMenu.classList.remove("display-on");
+  }
 });
+//
+//
+// Cick on Navigation Bar
+//
+const arrayNav = ["home", "team", "work", "news", "gallery", "contact"];
+const navItems = Array.from(document.querySelectorAll(".nav-item"));
 
-
-
-function scrollFromMenu(e){
-    let scrollY = e * window.innerHeight;
-    window.scrollTo({top: scrollY, behavior: 'smooth'})
+navItems.forEach((navItem) =>
+  navItem.addEventListener("click", (e) => {
+    if (window.location.pathname !== "/index.html") {
+      window.location.href =
+        "/index.html#" + arrayNav[navItems.indexOf(e.target)];
+    } else {
+      window.location.href = "#" + arrayNav[navItems.indexOf(e.target)];
     }
+  })
+);
 
-// 
-// 
+//
+//
 // CAROUSEL
 // from Splide
-// 
+//
 
-document.addEventListener( 'DOMContentLoaded', function () {
-    new Splide( '.splide',{
-        type : 'loop',
-        cover  : true,
-        height : '100vh',
-    } ).mount();
-} );
-// 
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide", {
+    type: "loop",
+    cover: true,
+    height: "100vh",
+  }).mount();
+});
+//
 // In & Out Articles & Form effect
 // (index page)
-// 
-const articles = document.querySelectorAll('.index-section article');
-const form = document.querySelector('form');
-const footer = document.querySelector('footer');
+//
+const articles = document.querySelectorAll(".index-section article");
+const form = document.querySelector("form");
+const footer = document.querySelector("footer");
 
 // Test resize Trigger
-window.addEventListener('resize', resetArticleTransformForSmartphone);
-function resetArticleTransformForSmartphone(){
-    if (window.innerWidth < 768){
+window.addEventListener("resize", resetArticleTransformForSmartphone);
+function resetArticleTransformForSmartphone() {
+  if (window.innerWidth < 768) {
     // Smartphone mode
-    articles.forEach(e => e.style.transform = 'none')    
-} else {
+    articles.forEach((e) => (e.style.transform = "none"));
+  } else {
     // Large screen Mode
-    articles.forEach(e => checkArticlePosition(e));
-}};
+    articles.forEach((e) => checkArticlePosition(e));
+  }
+}
 
 // Test Scoll Trigger
 // Article & Form in out
 window.addEventListener("scroll", () => {
-if (window.matchMedia("(min-width: 768px)").matches) {
+  if (window.matchMedia("(min-width: 768px)").matches) {
     checkFormPosition();
-    articles.forEach(e => checkArticlePosition(e));
-}
+    articles.forEach((e) => checkArticlePosition(e));
+  }
 });
 
+function checkArticlePosition(article) {
+  let fromTop = article.getBoundingClientRect().top;
+  let hWindow = window.innerHeight;
+  let hPercent = Math.floor((fromTop / hWindow) * 100);
+  if (hPercent < 80 && hPercent > 20) {
+    articleIn(article);
+  } else {
+    articleOut(article);
+  }
+}
+function articleIn(article) {
+  article.classList.contains("article-right")
+    ? (article.style.transform = "translate(-100%, -50%)")
+    : (article.style.transform = "translate(100%, -50%)");
+}
+function articleOut(article) {
+  article.style.transform = "translate(0, -50%)";
+}
 
-function checkArticlePosition(article){
-    let fromTop = article.getBoundingClientRect().top;
-    let hWindow = window.innerHeight;
-    let hPercent = Math.floor((fromTop / hWindow) * 100);
-    if (hPercent < 80 && hPercent > 20) {
-        articleIn(article);
-    } else {
-        articleOut(article);
-    };
-}
-function articleIn(article){
-    article.classList.contains('article-right') ? article.style.transform = 'translate(-100%, -50%)' : article.style.transform = 'translate(100%, -50%)';
-}
-function articleOut(article){
-    article.style.transform = 'translate(0, -50%)';
-}
-
-// 
+//
 // In & Out Form Scrolling effect
-// 
-function checkFormPosition(){
-//     const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
-//     if (scrollTop+clientHeight === scrollHeight) {
-//         form.style.transform = 'translate(0, -100%)';
-//     }
-// }
-let fromTop = footer.getBoundingClientRect().top;
-    let hWindow = window.innerHeight;
-    let hPercent = Math.floor((fromTop / hWindow) * 100);
-    if (hPercent < 95) {
-        form.style.transform = 'translate(0, 0)';
-    } else {
-        form.style.transform = 'translate(0, 100%)';
-    }
-};
+//
+function checkFormPosition() {  
+  let fromTop = footer.getBoundingClientRect().top;
+  let hWindow = window.innerHeight;
+  let hPercent = Math.floor((fromTop / hWindow) * 100);
+  if (hPercent < 95) {
+    form.style.transform = "translate(0, 0)";
+  } else {
+    form.style.transform = "translate(0, 100%)";
+  }
+}
